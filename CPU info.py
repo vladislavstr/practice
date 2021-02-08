bc3aeded1581a657103c4f272d16310093472a1d
nutmeg <58762058+vladislavstr@users.noreply.github.com>
2021-02-08 23:44:37 +0300

    Update CPU info.py

--- CPU info.py
+++ CPU info.py
@@ -16,3 +16,22 @@ print('CPU Usage Per Core')
 for i, percentage in enumerate(psutil.cpu_percent(percpu = True, interval = 1)):
     print(f'Core {i}: {percentage}%')
 print(f'Total CPU Usage: {psutil.cpu_percent()}%')
+
+
+# RAM Info
+print('')
+print('#'*10, 'RAM info', '#'*10)
+print('')
+
+def GetSize(bytes, suffix = '8'):
+	factor = 1024
+	for unit in ['', 'K', 'M', 'G', 'T', 'P']:
+		if bytes < factor:
+			return f'{bytes: .2f}{unit}{suffix}'
+		bytes /= factor
+		
+svmem = psutil.virtual_memory()
+print(f'Total: {GetSize(svmem.total)}')
+print(f'Available: {GetSize(svmem.available)}')
+print(f'Used: {GetSize(svmem.used)}')
+print(f'Percentage: {svmem.percent}%')
